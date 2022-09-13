2a21a60e45e558eff93e6ad0f0f9b413a9918340
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-09-13 13:56:27 -0300

    Implementada aba de navegação e informações do usuário

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -15,9 +15,13 @@ import axios from 'axios';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 
+import todayImage from '../../assets/imgs/today.jpg';
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg';
+import weekImage from '../../assets/imgs/week.jpg';
+import monthImage from '../../assets/imgs/month.jpg';
+
 import {server, showError} from '../common';
 import commonStyles from '../commonStyles';
-import todayImage from '../../assets/imgs/today.jpg';
 import Task from '../components/Task';
 import AddTask from './AddTask';
 
@@ -48,7 +52,9 @@ export default class TaskList extends Component {
 
   loadTasks = async () => {
     try {
-      const maxDate = moment().format('YYYY-MM-DD 23:59:59');
+      const maxDate = moment()
+        .add({days: this.props.daysAhead})
+        .format('YYYY-MM-DD 23:59:59');
       const res = await axios.get(`${server}/tasks?date=${maxDate}`);
       this.setState({tasks: res.data}, this.filterTasks);
     } catch (e) {
@@ -114,8 +120,35 @@ export default class TaskList extends Component {
     }
   };
 
+  getImage = () => {
+    switch (this.props.daysAhead) {
+      case 0:
+        return todayImage;
+      case 1:
+        return tomorrowImage;
+      case 7:
+        return weekImage;
+      case 30:
+        return monthImage;
+    }
+  };
+
+  getColor = () => {
+    switch (this.props.daysAhead) {
+      case 0:
+        return commonStyles.colors.today;
+      case 1:
+        return commonStyles.colors.tomorrow;
+      case 7:
+        return commonStyles.colors.week;
+      case 30:
+        return commonStyles.colors.month;
+    }
+  };
+
   render() {
     const today = moment()
+      .add({days: this.props.daysAhead})
       .locale('pt-br')
       .format('ddd, D [de] MMMM');
 
@@ -126,8 +159,16 @@ export default class TaskList extends Component {
           onCancel={() => this.setState({showAddTask: false})}
           onSave={this.addTask}
         />
-        <ImageBackground source={todayImage} style={styles.background}>
+        <ImageBackground source={this.getImage()} style={styles.background}>
           <View style={styles.iconBar}>
+            <TouchableOpacity
+              onPress={() => this.props.navigation.openDrawer()}>
+              <Icon
+                name="bars"
+                size={20}
+                color={commonStyles.colors.secondary}
+              />
+            </TouchableOpacity>
             <TouchableOpacity onPress={this.toggleFilter}>
               <Icon
                 name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
@@ -137,7 +178,7 @@ export default class TaskList extends Component {
             </TouchableOpacity>
           </View>
           <View style={styles.titleBar}>
-            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.title}>{this.props.title}</Text>
             <Text style={styles.subtitle}>{today}</Text>
           </View>
         </ImageBackground>
@@ -156,7 +197,7 @@ export default class TaskList extends Component {
         </View>
         <TouchableOpacity
           activeOpacity={0.7}
-          style={styles.addButton}
+          style={[styles.addButton, {backgroundColor: this.getColor()}]}
           onPress={() => this.setState({showAddTask: true})}>
           <Icon name="plus" size={20} color={commonStyles.colors.secondary} />
         </TouchableOpacity>
@@ -196,7 +237,7 @@ const styles = StyleSheet.create({
   iconBar: {
     flexDirection: 'row',
     marginHorizontal: 20,
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
     marginTop: Platform.OS === 'ios' ? 30 : 10,
   },
   addButton: {
@@ -206,7 +247,6 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 25,
-    backgroundColor: commonStyles.colors.today,
     justifyContent: 'center',
     alignItems: 'center',
   },
